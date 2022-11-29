cad5785a8058cc96994c2f519fe9c81684b4fd56
vrfjr <vagnerrfjr@gmail.com>
2022-11-29 18:16:58 -0300

    29do11

--- Program.cs
+++ Program.cs
@@ -2,11 +2,69 @@
 using System;
 using System.Globalization;
 
-CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("pt_BR");
 
-decimal valorMonetario = 82.40M;
+// data
 
-Console.WriteLine(valorMonetario.ToString("C", CultureInfo.CreateSpecificCulture("en-US")));
+string dataString = "2022-12-17 18:00";
+
+bool sucesso = DateTime.TryParseExact(dataString, "yyyy-MM-dd HH:mm", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime data);
+
+////// DateTime.TryParseExact(string, formatado que esta string, cultura ou padrao, estilo, variavel que ele vai jogar);
+
+if (sucesso)
+{
+    Console.WriteLine($"Conversão com sucesso! Data: {data}");
+}
+else
+{
+    Console.WriteLine($"{dataString} não é uma data valida");
+}
+
+
+
+
+
+// DateTime data = DateTime.Parse("17/04/2022 18:00");
+
+// Console.WriteLine(data);
+
+
+// Console.WriteLine(data.ToShortTimeString());
+
+// Console.WriteLine(data.ToShortDateString());
+
+// Console.WriteLine(data.ToString("dd/MM/yyyy HH:mm"));
+
+// Console.WriteLine(data.ToString("dd-MM-yyyy HH:mm"));
+
+
+
+
+
+
+
+
+
+
+// Formatação
+
+// CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("pt_BR");
+
+// decimal valorMonetario = 82.40M;
+
+
+// Console.WriteLine(valorMonetario.ToString("C2"));
+
+
+// double porcentagem = .3421;
+
+// Console.WriteLine(porcentagem.ToString("P"));
+
+// int numero = 123456;
+
+// Console.WriteLine(numero.ToString("##-##-##"));
+
+// Console.WriteLine(valorMonetario.ToString("C", CultureInfo.CreateSpecificCulture("en-US")));
 
 
 
